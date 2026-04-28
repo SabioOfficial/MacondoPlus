0844897e0557c10e89393fb9d708f4e86e1b3b67
Sabio <gamester.sad@gmail.com>
2026-04-28 19:51:56 +0100

    add module descriptions

--- extension/scripts/content-script.js
+++ extension/scripts/content-script.js
@@ -1,10 +1,10 @@
 (() => {
   const PREFIX = "macondoplus_module_";
   const MODULES = [
-    {id: "better-page-titles", name: "Better Page Titles", description: "Temporary description: to be replaced", defaultEnabled: true},
-    {id: "dark-mode", name: "Dark Mode", description: "Temporary description: to be replaced", defaultEnabled: false},
-    {id: "hide-feedback", name: "Hide Feedback", description: "Temporary description: to be replaced", defaultEnabled: true},
-    {id: "project-search", name: "Project Search", description: "Temporary description: to be replaced", defaultEnabled: true},
+    {id: "better-page-titles", name: "Better Page Titles", description: "Makes the page titles useful", defaultEnabled: true},
+    {id: "dark-mode", name: "Dark Mode", description: "Adds dark mode to Macondo (WIP)", defaultEnabled: false},
+    {id: "hide-feedback", name: "Hide Feedback", description: "Hides the annoying NPS (feedback) button", defaultEnabled: true},
+    {id: "project-search", name: "Project Search", description: "Adds a way to search for projects", defaultEnabled: true},
   ];
   
   function isEnabled(id) {
