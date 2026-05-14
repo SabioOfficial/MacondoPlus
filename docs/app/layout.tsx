940ca8ec7f4c7e966023d61d26b7eebfaa233d27
Sabio <gamester.sad@gmail.com>
2026-05-14 22:14:09 +0100

    add basic website

---
+++ docs/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Macondo+",
+  description: "Macondo+ (or MacondoPlus) is a QoL-focused browser extension for Chrome and Firefox that improves the Macondo website drastically. From dark mode to keyboard shortcuts, from shop goals to displaying your progress on securing your streak for the day, we've got you covered!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ docs/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-row gap-2 justify-center items-center">
+      <a href="https://chromewebstore.google.com/detail/macondo+/ldhbamehlholbmcfmihlhagjpdkmjlgo" target="_blank" className="btn">Chrome Web Store</a>
+      <a href="https://addons.mozilla.org/en-US/firefox/addon/macondo/" target="_blank" className="btn">Firefox Add-ons</a>
+    </div>
+  );
+}
